672eb8dc39fabc56ec7379eaa89b7612a553955f
Nic30 <nic30@seznam.cz>
2016-08-01 17:19:20 +0200

    force use newly compilled module

--- tests/test.py
+++ tests/test.py
@@ -3,14 +3,14 @@ from os import path
 from  pprint import pprint
 
 BASE_DIR = path.join(path.dirname(__file__), "..")
-sys.path.append(path.join(BASE_DIR, "build/lib.linux-x86_64-3.5")) 
+sys.path.insert(1, path.join(BASE_DIR, "build/lib.linux-x86_64-3.5")) 
 
 import hdlConvertor
-f = path.join(BASE_DIR, "tests/uart.v")
-res =  hdlConvertor.parse(f, "verilog", debug=True)
-pprint(res)
 
 f = path.join(BASE_DIR, "tests/mux.vhd")
-res =  hdlConvertor.parse(f, "vhdl", debug=True)
+res = hdlConvertor.parse(f, "vhdl", debug=True)
 pprint(res)
 
+f = path.join(BASE_DIR, "tests/uart.v")
+res = hdlConvertor.parse(f, "verilog", debug=True)
+pprint(res)
