8429b1e094147e6de76d0a4c27c686b66dc9d0ce
Nic30 <nic30@seznam.cz>
2017-10-30 19:19:38 +0100

    mv grammars to dedicated dir

--- generateGram.sh
+++ generateGram.sh
@@ -2,7 +2,7 @@
 set -o errexit
 
 ANTLR_VERSION="4.7"
-ANTLR_JAR="antlr-$ANTLR_VERSION-complete.jar":
+ANTLR_JAR="antlr-$ANTLR_VERSION-complete.jar"
 ANTLR_RUNTIME="antlr4-cpp-runtime-$ANTLR_VERSION-source.zip"
 
 if [ ! -f $ANTLR_JAR ]; then
@@ -18,9 +18,9 @@ fi
 # rm -r src/antlr4-runtime-cpp/runtime
 
 antlr4_createParser="java -Xmx500M -cp \"$ANTLR_JAR:$CLASSPATH\" org.antlr.v4.Tool -Dlanguage=Cpp -visitor -no-listener"
-eval "$antlr4_createParser vhdl.g4 -o VhdlParser -package vhdl"
-eval "$antlr4_createParser Verilog2001.g4 -o VerilogParser -package verilog"
-eval "$antlr4_createParser sv2012.g4 -o SVParser -package sv"
+eval "$antlr4_createParser grammars/vhdl.g4 -o VhdlParser -package vhdl"
+eval "$antlr4_createParser grammars/Verilog2001.g4 -o VerilogParser -package verilog"
+eval "$antlr4_createParser grammars/sv2012.g4 -o SVParser -package sv"
 
 rm -rf src/VhdlParser 
 mv  VhdlParser src/
--- src/VerilogParser/grammars/Verilog2001Parser.cpp
+++ src/VerilogParser/grammars/Verilog2001Parser.cpp
@@ -1,5 +1,5 @@
 
-// Generated from Verilog2001.g4 by ANTLR 4.7
+// Generated from grammars/Verilog2001.g4 by ANTLR 4.7
 
 
 #include "Verilog2001Visitor.h"
