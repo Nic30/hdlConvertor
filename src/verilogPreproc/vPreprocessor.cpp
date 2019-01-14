27a8e6a79e2a969d4eab7847db1a52f09bf039fe
nic30 <nic30@seznam.cz>
2019-01-14 19:40:23 +0100

    autoformat

--- src/verilogPreproc/vPreprocessor.cpp
+++ src/verilogPreproc/vPreprocessor.cpp
@@ -4,10 +4,9 @@ macroSymbol vPreprocessor::_defineDB;
 std::vector<std::string> vPreprocessor::_stack_incfile;
 
 vPreprocessor::vPreprocessor(TokenStream * tokens,
-		std::vector<std::string> &incdir,
-		bool eraseDB) {
+		std::vector<std::string> &incdir, bool eraseDB) {
 	_rewriter = new TokenStreamRewriter(tokens);
-	_tokens =  (CommonTokenStream *) tokens;
+	_tokens = (CommonTokenStream *) tokens;
 	_incdir = incdir;
 	if (eraseDB == true) {
 		_defineDB.clear();
@@ -21,42 +20,43 @@ vPreprocessor::~vPreprocessor() {
 
 std::string vPreprocessor::genBlank(size_t n) {
 	std::string a_string;
-	a_string.resize(n,' ');
+	a_string.resize(n, ' ');
 	return a_string;
 }
 
-void vPreprocessor::enterDefine(verilogPreprocParser::DefineContext * ctx){
+void vPreprocessor::enterDefine(verilogPreprocParser::DefineContext * ctx) {
 	std::string macroName = ctx->macro_id()->getText();
-	if (ctx->ID().size() !=0 ) {
+	if (ctx->ID().size() != 0) {
 		std::vector<std::string> data;
-		for(auto arg : ctx->ID()) {
+		for (auto arg : ctx->ID()) {
 			data.push_back(arg->getText());
 		}
 		std::string rep_data = ctx->replacement()->getText();
 
-		macro_replace * item  = new macro_replace(rep_data,data);
-		_defineDB.insert(std::pair<std::string,macro_replace*>(macroName,item),_incdir);
-	}
-	else {
+		macro_replace * item = new macro_replace(rep_data, data);
+		_defineDB.insert(
+				std::pair<std::string, macro_replace*>(macroName, item),
+				_incdir);
+	} else {
 		std::vector<std::string> data;
-	        if (ctx->replacement() != NULL) {
+		if (ctx->replacement() != NULL) {
 			std::string rep_data = ctx->replacement()->getText();
-			macro_replace *item = new macro_replace(rep_data,data);
-			_defineDB.insert(std::pair<std::string,macro_replace*>(macroName,item),_incdir);
-		}
-		else {
-			macro_replace *item = new macro_replace("",data);
-			_defineDB.insert(std::pair<std::string,macro_replace*>(macroName,item),_incdir);
+			macro_replace *item = new macro_replace(rep_data, data);
+			_defineDB.insert(
+					std::pair<std::string, macro_replace*>(macroName, item),
+					_incdir);
+		} else {
+			macro_replace *item = new macro_replace("", data);
+			_defineDB.insert(
+					std::pair<std::string, macro_replace*>(macroName, item),
+					_incdir);
 		}
 		//_defineDB[macroName] = item;
 	}
 
 	misc::Interval token = ctx->getSourceInterval();
 
-	_rewriter->replace(
-				token.a,
-				token.b,
-				std::string(""));
+	_rewriter->replace(token.a, token.b, std::string(""));
 }
 
 void vPreprocessor::enterUndef(verilogPreprocParser::UndefContext * ctx) {
@@ -64,7 +64,8 @@ void vPreprocessor::enterUndef(verilogPreprocParser::UndefContext * ctx) {
 }
 
 void vPreprocessor::enterToken_id(verilogPreprocParser::Token_idContext * ctx) {
-	macroPrototype macro = return_prototype(ctx->getText().substr(1,std::string::npos));
+	macroPrototype macro = return_prototype(
+			ctx->getText().substr(1, std::string::npos));
 
 	if (_defineDB.find(macro.macroName) == _defineDB.end()) {
 		std::string msg = macro.macroName + "is not defined";
@@ -74,14 +75,11 @@ void vPreprocessor::enterToken_id(verilogPreprocParser::Token_idContext * ctx) {
 
 	misc::Interval token = ctx->getSourceInterval();
 
-	_rewriter->replace(
-				token.a,
-				token.b,
-				replacement);
+	_rewriter->replace(token.a, token.b, replacement);
 }
 
-
-void vPreprocessor::exitIfdef_directive(verilogPreprocParser::Ifdef_directiveContext * ctx) {
+void vPreprocessor::exitIfdef_directive(
+		verilogPreprocParser::Ifdef_directiveContext * ctx) {
 
 	uint32_t ID_cpt = 0;
 	macroSymbol::iterator search;
@@ -89,35 +87,40 @@ void vPreprocessor::exitIfdef_directive(verilogPreprocParser::Ifdef_directiveCon
 	misc::Interval token = ctx->getSourceInterval();
 
 	search = _defineDB.find(ctx->ID(0)->getText());
-	if (search != _defineDB.end() ) {
-		misc::Interval interval = ctx->ifdef_group_of_lines()->getSourceInterval();
-		replacement = return_preprocessed(_tokens->getText(interval),_incdir,false);
-	}
-	else {
+	if (search != _defineDB.end()) {
+		misc::Interval interval =
+				ctx->ifdef_group_of_lines()->getSourceInterval();
+		replacement = return_preprocessed(_tokens->getText(interval), _incdir,
+				false);
+	} else {
 		ID_cpt++;
-		while (ID_cpt < ctx->ID().size() ) {
+		while (ID_cpt < ctx->ID().size()) {
 			search = _defineDB.find(ctx->ID(ID_cpt)->getText());
-			if (search != _defineDB.end() ) {
-				misc::Interval interval = ctx->elsif_group_of_lines(ID_cpt)->getSourceInterval();
-				replacement = return_preprocessed(_tokens->getText(interval),_incdir,false);
+			if (search != _defineDB.end()) {
+				misc::Interval interval =
+						ctx->elsif_group_of_lines(ID_cpt)->getSourceInterval();
+				replacement = return_preprocessed(_tokens->getText(interval),
+						_incdir, false);
 				goto exit_label;
 
 			}
 			ID_cpt++;
 		}
 		if (ctx->ELSE() != nullptr) {
-			misc::Interval interval = ctx->else_group_of_lines()->getSourceInterval();
-			replacement = return_preprocessed(_tokens->getText(interval),_incdir,false);
+			misc::Interval interval =
+					ctx->else_group_of_lines()->getSourceInterval();
+			replacement = return_preprocessed(_tokens->getText(interval),
+					_incdir, false);
 
 		}
 	}
 
-	exit_label:
-	_rewriter->replace(token.a,token.b,replacement);
+	exit_label: _rewriter->replace(token.a, token.b, replacement);
 
 }
 
-void vPreprocessor::exitIfndef_directive(verilogPreprocParser::Ifndef_directiveContext * ctx) {
+void vPreprocessor::exitIfndef_directive(
+		verilogPreprocParser::Ifndef_directiveContext * ctx) {
 
 	uint32_t ID_cpt = 0;
 	macroSymbol::iterator search;
@@ -125,31 +128,35 @@ void vPreprocessor::exitIfndef_directive(verilogPreprocParser::Ifndef_directiveC
 	misc::Interval token = ctx->getSourceInterval();
 
 	search = _defineDB.find(ctx->ID(0)->getText());
-	if (search != _defineDB.end() ) {
-		misc::Interval interval = ctx->ifndef_group_of_lines()->getSourceInterval();
-		replacement = return_preprocessed(_tokens->getText(interval),_incdir,false);
-	}
-	else {
+	if (search != _defineDB.end()) {
+		misc::Interval interval =
+				ctx->ifndef_group_of_lines()->getSourceInterval();
+		replacement = return_preprocessed(_tokens->getText(interval), _incdir,
+				false);
+	} else {
 		ID_cpt++;
-		while (ID_cpt < ctx->ID().size() ) {
+		while (ID_cpt < ctx->ID().size()) {
 			search = _defineDB.find(ctx->ID(ID_cpt)->getText());
-			if (search != _defineDB.end() ) {
-				misc::Interval interval = ctx->elsif_group_of_lines(ID_cpt)->getSourceInterval();
-				replacement = return_preprocessed(_tokens->getText(interval),_incdir,false);
+			if (search != _defineDB.end()) {
+				misc::Interval interval =
+						ctx->elsif_group_of_lines(ID_cpt)->getSourceInterval();
+				replacement = return_preprocessed(_tokens->getText(interval),
+						_incdir, false);
 				goto exit_label;
 
 			}
 			ID_cpt++;
 		}
 		if (ctx->ELSE() != nullptr) {
-			misc::Interval interval = ctx->else_group_of_lines()->getSourceInterval();
-			replacement = return_preprocessed(_tokens->getText(interval),_incdir,false);
+			misc::Interval interval =
+					ctx->else_group_of_lines()->getSourceInterval();
+			replacement = return_preprocessed(_tokens->getText(interval),
+					_incdir, false);
 
 		}
 	}
 
-	exit_label:
-	_rewriter->replace(token.a,token.b,replacement);
+	exit_label: _rewriter->replace(token.a, token.b, replacement);
 }
 
 void vPreprocessor::enterInclude(verilogPreprocParser::IncludeContext * ctx) {
@@ -157,63 +164,62 @@ void vPreprocessor::enterInclude(verilogPreprocParser::IncludeContext * ctx) {
 	struct stat buffer;
 
 	std::vector<std::string>::iterator incdir_iter = _incdir.begin();
-	bool found=false;
+	bool found = false;
 	std::string StringLiteral = ctx->StringLiteral()->getText();
 	std::string filename;
-	while(incdir_iter != _incdir.end() && found == false){
+	while (incdir_iter != _incdir.end() && found == false) {
 
-		filename = (*incdir_iter) + '/' + StringLiteral.substr(1,StringLiteral.size()-2);
-		if (stat(filename.c_str(),&buffer) == 0) {
+		filename = (*incdir_iter) + '/'
+				+ StringLiteral.substr(1, StringLiteral.size() - 2);
+		if (stat(filename.c_str(), &buffer) == 0) {
 			found = true;
 		}
 		incdir_iter++;
 	}
 	if (found == false) {
-		std::string msg = StringLiteral.substr(1,StringLiteral.size()-2) + \
-			" was not found in include directory\n";
+		std::string msg = StringLiteral.substr(1, StringLiteral.size() - 2)
+				+ " was not found in include directory\n";
 		throw parseException(msg);
-	}
-	else if (_stack_incfile.size() > 20) {
+	} else if (_stack_incfile.size() > 20) {
 		std::string msg = "Nested include limite reach";
 		throw parseException(msg);
-	}
-	else {
+	} else {
 
 		misc::Interval token = ctx->getSourceInterval();
 		//std::string replacement = genBlank(ctx->getText().size());
 		std::string replacement;
-		_rewriter->Delete(token.a,token.b);
+		_rewriter->Delete(token.a, token.b);
 
 		_stack_incfile.push_back(filename);
-  		std::ifstream t(filename);
-  		std::string str((std::istreambuf_iterator<char>(t)),
-                	std::istreambuf_iterator<char>());
+		std::ifstream t(filename);
+		std::string str((std::istreambuf_iterator<char>(t)),
+				std::istreambuf_iterator<char>());
 
-                replacement = return_preprocessed(str,_incdir,false);
+		replacement = return_preprocessed(str, _incdir, false);
 		_stack_incfile.pop_back();
-		_rewriter->insertAfter(ctx->StringLiteral()->getSymbol(),replacement);
+		_rewriter->insertAfter(ctx->StringLiteral()->getSymbol(), replacement);
 
 	}
 
 }
 
+std::string return_preprocessed(const std::string input_token,
+		std::vector<std::string> &incdir, bool eraseDB) {
 
-std::string return_preprocessed(const std::string input_token,std::vector<std::string> &incdir , bool eraseDB) {
+	ANTLRInputStream input(input_token);
+	verilogPreprocLexer * lexer = new verilogPreprocLexer(&input);
+	CommonTokenStream * tokens = new CommonTokenStream(lexer);
+	verilogPreprocParser * parser = new verilogPreprocParser(tokens);
+	tree::ParseTree *tree = parser->file();
 
-  ANTLRInputStream input(input_token);
-  verilogPreprocLexer * lexer = new verilogPreprocLexer(&input);
-  CommonTokenStream * tokens = new CommonTokenStream(lexer);
-  verilogPreprocParser * parser = new verilogPreprocParser(tokens);
-  tree::ParseTree *tree = parser->file();
-
-  tree::ParseTreeWalker walker = tree::ParseTreeWalker();
-  vPreprocessor * extractor = new vPreprocessor(tokens,incdir,eraseDB);
-  walker.walk( (tree::ParseTreeListener*) extractor,tree);
-  std::string return_value = extractor->_rewriter->getText();
-  delete extractor;
-  delete parser;
-  delete tokens;
-  delete lexer;
-  return return_value;
+	tree::ParseTreeWalker walker = tree::ParseTreeWalker();
+	vPreprocessor * extractor = new vPreprocessor(tokens, incdir, eraseDB);
+	walker.walk((tree::ParseTreeListener*) extractor, tree);
+	std::string return_value = extractor->_rewriter->getText();
+	delete extractor;
+	delete parser;
+	delete tokens;
+	delete lexer;
+	return return_value;
 
 }
