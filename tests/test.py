c7ad82db3dc16958a18bf01b83f372114e2c1fe4
Thomas <thomas.burg@gmail.com>
2019-01-23 21:40:25 +0100

    Add directive_verilogpp

--- tests/test.py
+++ tests/test.py
@@ -98,6 +98,9 @@ class BasicTC(unittest.TestCase):
         f, res = dumpFile("macro.v",'verilog')
         str(res)
 
+    def test_directive_verilogpp(self):
+        f, res = dumpFile("directive_verilogpp.v",'verilog')
+        str(res)
 
 #    def test_system_verilog_mem_base_object(self):
 #        f, res = dumpFile("mem_base_object.sv",  "systemVerilog")
