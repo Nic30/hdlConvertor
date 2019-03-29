6b601e99046c11b6ea4790485753b48247c72c09
nic30 <nic30@seznam.cz>
2019-03-29 02:46:20 +0000

    codestyle/doc

--- src/hdlObjects/compInstance.cpp
+++ src/hdlObjects/compInstance.cpp
@@ -4,6 +4,7 @@ CompInstance::CompInstance(char * _name, Expr * _entityName) {
 	entityName = _entityName;
 	name = _name;
 }
+
 #ifdef USE_PYTHON
 PyObject * CompInstance::toJson() const {
 	PyObject * d = Named::toJson();
