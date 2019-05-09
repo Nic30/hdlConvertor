e40ea2ebb24d41e9ae2b0bd5b6443322e2adc670
nic30 <nic30@seznam.cz>
2019-05-09 16:36:05 +0100

    add missing items to component instance json

--- src/hdlObjects/compInstance.cpp
+++ src/hdlObjects/compInstance.cpp
@@ -1,4 +1,5 @@
 #include "compInstance.h"
+#include "../hdlObjects/symbolType.h"
 
 CompInstance::CompInstance(Expr * _name, Expr * _entityName) {
 	entityName = _entityName;
@@ -8,8 +9,20 @@ CompInstance::CompInstance(Expr * _name, Expr * _entityName) {
 #ifdef USE_PYTHON
 PyObject * CompInstance::toJson() const {
 	PyObject * d = WithDoc::toJson();
-	PyDict_SetItemString(d, "name", name->toJson());
+	char * name_str = nullptr;
+	Symbol * name_lit = dynamic_cast<Symbol*>(name->data);
+	if (name_lit and name_lit->type == SymbolType::symb_ID) {
+		name_str = name_lit->value._str;
+	}
+	if (name_str)
+		PyDict_SetItemString(d, "name", PyUnicode_FromString(name_str));
+	else
+		PyDict_SetItemString(d, "name", name->toJson());
+
 	PyDict_SetItemString(d, "entityName", entityName->toJson());
+	addJsonArrP(d, "portMap", portMap);
+	addJsonArrP(d, "genericMap", genericMap);
+
 	return d;
 }
 #endif
