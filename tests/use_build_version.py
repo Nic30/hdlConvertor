a3433905d7b5c0bade81cbba7e0e08817bb1d490
nic30 <nic30@seznam.cz>
2019-05-11 03:30:56 +0100

    rm debug

--- tests/use_build_version.py
+++ tests/use_build_version.py
@@ -5,7 +5,6 @@ TEST_DIR = os.path.abspath(os.path.dirname(__file__))
 BASE_DIR = os.path.join(TEST_DIR, "..")
 SKBUILD_DIR = os.path.join(BASE_DIR, "_skbuild")
 # there should be only one directory with the name of the python configuration like linux-x86_64-3.7
-print(SKBUILD_DIR)
 PY_CONFIG = os.listdir(SKBUILD_DIR)[0]
 SKBUILD_BUILD_DIR = os.path.join(SKBUILD_DIR, PY_CONFIG, "setuptools", "lib")
 
