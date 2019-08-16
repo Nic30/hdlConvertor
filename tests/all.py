c64ddab9039b810b5448d5b37e1a2c1a0d4334cf
nic30 <nic30@seznam.cz>
2019-08-17 01:13:19 +0200

    add SvStdExamplesParseTC to all tests

--- tests/all.py
+++ tests/all.py
@@ -7,12 +7,14 @@ try:
     from tests.test_verilog_preproc_include import VerilogPreprocIncludeTC
     from tests.test_verilog_conversion import VerilogConversionTC
     from tests.test_vhdl_conversion import VhdlConversionTC
+    from tests.test_sv_std_examples_parse import SvStdExamplesParseTC
 except ImportError:
     from test_verilog_preproc import VerilogPreprocTC
     from test_verilog_preproc_grammar import VerilogPreprocGrammarTC
     from test_verilog_preproc_include import VerilogPreprocIncludeTC
     from test_verilog_conversion import VerilogConversionTC
     from test_vhdl_conversion import VhdlConversionTC
+    from test_sv_std_examples_parse import SvStdExamplesParseTC
 
 
 if __name__ == "__main__":
@@ -23,6 +25,7 @@ if __name__ == "__main__":
         VerilogPreprocIncludeTC,
         VerilogConversionTC,
         VhdlConversionTC,
+        SvStdExamplesParseTC,
     ]
     for tc in tcs:
         suite.addTest(unittest.makeSuite(tc))
