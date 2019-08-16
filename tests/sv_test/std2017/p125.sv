0157a8aa10a3f3f950407abac6a58a8d32e63dbf
nic30 <nic30@seznam.cz>
2019-08-16 21:59:31 +0200

    sv2017: example p125: add missing module wrap

--- tests/sv_test/std2017/p125.sv
+++ tests/sv_test/std2017/p125.sv
@@ -1,4 +1,8 @@
+module top;
+
 specify
-specparam tRise_clk_q = 150, tFall_clk_q = 200;
-specparam tRise_control = 40, tFall_control = 50;
+	specparam tRise_clk_q = 150, tFall_clk_q = 200;
+	specparam tRise_control = 40, tFall_control = 50;
 endspecify
+
+endmodule;
