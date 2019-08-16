7e7e41bf41cf3a534ea3bfb5b9eeb2767adac91c
nic30 <nic30@seznam.cz>
2019-08-16 22:01:46 +0200

    sv2017: example p541: add missing module wrap

--- tests/sv_test/std2017/p541.sv
+++ tests/sv_test/std2017/p541.sv
@@ -1,14 +1,18 @@
 class C1;
-rand integer x;
+	rand integer x;
 endclass
+
 class C2;
-rand integer y;
+	rand integer y;
 endclass
+
+module top;
 initial begin
 	C1 c1 = new();
-C2 c2 = new();
-integer z;
-void'(c1.randomize());
-// z = $random;
-void'(c2.randomize());
+	C2 c2 = new();
+	integer z;
+	void'(c1.randomize());
+	// z = $random;
+	void'(c2.randomize());
 end
+endmodule
