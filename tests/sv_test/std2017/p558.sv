b73a580ebf1196fc62843b05eed55641cdf3157e
nic30 <nic30@seznam.cz>
2019-08-16 22:02:51 +0200

    sv2017: example p558: add missing module wrap

--- tests/sv_test/std2017/p558.sv
+++ tests/sv_test/std2017/p558.sv
@@ -1,13 +1,16 @@
 class C1;
-bit [7:0] x;
-covergroup cv (int arg) @(posedge clk);
-option.at_least = arg;
-coverpoint x;
-endgroup
-function new(int p1);
-cv = new(p1);
-endfunction
+	bit [7:0] x;
+	covergroup cv (int arg) @(posedge clk);
+		option.at_least = arg;
+		coverpoint x;
+	endgroup
+	function new(int p1);
+		cv = new(p1);
+	endfunction
 endclass
-initial begin
-C1 obj = new(4);
-end
+
+module top;
+	initial begin
+		C1 obj = new(4);
+	end
+endmodule
