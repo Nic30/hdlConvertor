d7f20a6d15bbd8b4005e9ee521090382853129bd
nic30 <nic30@seznam.cz>
2019-10-10 12:57:12 +0200

    conversion of system verilog function/tasks

--- include/hdlConvertor/svConvertor/programParser.h
+++ include/hdlConvertor/svConvertor/programParser.h
@@ -13,6 +13,11 @@ public:
 	using sv2017Parser = sv2017_antlr::sv2017Parser;
 	VerProgramParser(SVCommentParser &commentParser);
 
+	void visitTf_port_declaration(sv2017Parser::Tf_port_declarationContext *ctx,
+			std::vector<std::unique_ptr<hdlObjects::HdlVariableDef>> &res);
+	void visitTf_item_declaration(sv2017Parser::Tf_item_declarationContext *ctx,
+			std::vector<std::unique_ptr<hdlObjects::iHdlObj>> &objs,
+			std::vector<std::unique_ptr<hdlObjects::HdlVariableDef>> &ports);
 	std::unique_ptr<hdlObjects::HdlFunctionDef> visitTask_and_function_declaration_common(
 			sv2017Parser::Task_and_function_declaration_commonContext *ctx,
 			std::unique_ptr<hdlObjects::iHdlExpr> return_t, bool is_static,
--- src/svConvertor/programParser.cpp
+++ src/svConvertor/programParser.cpp
@@ -3,10 +3,12 @@
 #include <hdlConvertor/notImplementedLogger.h>
 
 #include <hdlConvertor/svConvertor/typeParser.h>
+#include <hdlConvertor/svConvertor/portParser.h>
 #include <hdlConvertor/svConvertor/exprParser.h>
 #include <hdlConvertor/svConvertor/statementParser.h>
 #include <hdlConvertor/svConvertor/portParser.h>
 #include <hdlConvertor/svConvertor/declrParser.h>
+#include <hdlConvertor/svConvertor/attributeParser.h>
 
 using namespace std;
 using sv2017Parser = sv2017_antlr::sv2017Parser;
@@ -17,6 +19,47 @@ namespace sv {
 
 VerProgramParser::VerProgramParser(SVCommentParser &_commentParser) :
 		commentParser(_commentParser) {
+}
+
+void VerProgramParser::visitTf_item_declaration(
+		sv2017Parser::Tf_item_declarationContext *ctx,
+		vector<unique_ptr<iHdlObj>> &objs,
+		vector<unique_ptr<HdlVariableDef>> &ports) {
+	// tf_item_declaration:
+	//     block_item_declaration
+	//     | tf_port_declaration
+	// ;
+	auto bid = ctx->block_item_declaration();
+	if (bid)
+		VerStatementParser(commentParser).visitBlock_item_declaration(bid,
+				objs);
+	else {
+		auto tpd = ctx->tf_port_declaration();
+		visitTf_port_declaration(tpd, ports);
+	}
+}
+
+void VerProgramParser::visitTf_port_declaration(
+		sv2017Parser::Tf_port_declarationContext *ctx,
+		vector<unique_ptr<HdlVariableDef>> &res) {
+	// tf_port_declaration:
+	//     ( attribute_instance )* tf_port_direction ( KW_VAR )? ( data_type_or_implicit )?
+	//     list_of_tf_variable_identifiers SEMI
+	// ;
+	VerAttributeParser::visitAttribute_instance(ctx->attribute_instance());
+	auto _tpd = ctx->tf_port_direction();
+	std::vector<VerPortParser::Non_ANSI_port_info_t> non_ansi_port_groups;
+	VerPortParser pp(commentParser, non_ansi_port_groups);
+	auto pd = pp.visitTf_port_direction(_tpd);
+	if (ctx->KW_VAR())
+		pd = HdlDirection::DIR_LINKAGE;
+	VerTypeParser tp(commentParser);
+	auto dti = ctx->data_type_or_implicit();
+	auto t = tp.visitData_type_or_implicit(dti, nullptr);
+	auto lvi = ctx->list_of_tf_variable_identifiers();
+	auto doc = commentParser.parse(ctx);
+	pp.visitList_of_tf_variable_identifiers(lvi, move(t), false, pd, doc, res);
+	assert(non_ansi_port_groups.size() == 0);
 }
 
 std::unique_ptr<HdlFunctionDef> VerProgramParser::visitTask_and_function_declaration_common(
@@ -45,20 +88,19 @@ std::unique_ptr<HdlFunctionDef> VerProgramParser::visitTask_and_function_declara
 		name = VerExprParser::getIdentifierStr(ids[0]);
 	}
 	auto params = make_unique<std::vector<unique_ptr<HdlVariableDef>>>();
-	auto f = make_unique<HdlFunctionDef>(name, false, move(return_t), move(params));
+	auto f = make_unique<HdlFunctionDef>(name, false, move(return_t),
+			move(params));
 	f->is_static = is_static;
 	f->is_task = is_task;
 	f->is_declaration_only = false;
 	std::vector<VerPortParser::Non_ANSI_port_info_t> non_ansi_port_groups;
-	if (ctx->tf_item_declaration().size()) {
-		NotImplementedLogger::print(
-				"VerProgramParser.visitTask_and_function_declaration_common class tf_item_declaration",
-				ctx->tf_item_declaration()[0]);
-	} else {
 		auto pl = ctx->tf_port_list();
-		if (pl) {
-			VerPortParser pp(commentParser, non_ansi_port_groups);
-			pp.visitTf_port_list(pl, *f->params);
+	if (pl) {
+		VerPortParser pp(commentParser, non_ansi_port_groups);
+				pp.visitTf_port_list(pl, *f->params);
+	} else {
+		for (auto tfp: ctx->tf_item_declaration()) {
+			visitTf_item_declaration(tfp, f->body, *f->params);
 		}
 	}
 	VerStatementParser sp(commentParser);
@@ -89,8 +131,8 @@ std::unique_ptr<HdlFunctionDef> VerProgramParser::visitTask_declaration(
 	bool is_static = tp.visitLifetime(ctx->lifetime());
 	auto return_t = iHdlExpr::ID("void");
 	auto tfcom = ctx->task_and_function_declaration_common();
-	return visitTask_and_function_declaration_common(tfcom, move(return_t), is_static,
-			true);
+	return visitTask_and_function_declaration_common(tfcom, move(return_t),
+			is_static, true);
 }
 
 std::unique_ptr<HdlFunctionDef> VerProgramParser::visitFunction_declaration(
@@ -106,8 +148,8 @@ std::unique_ptr<HdlFunctionDef> VerProgramParser::visitFunction_declaration(
 	auto fdti = ctx->function_data_type_or_implicit();
 	auto return_t = tp.visitFunction_data_type_or_implicit(fdti);
 	auto tfcom = ctx->task_and_function_declaration_common();
-	return visitTask_and_function_declaration_common(tfcom, std::move(return_t), is_static,
-			false);
+	return visitTask_and_function_declaration_common(tfcom, std::move(return_t),
+			is_static, false);
 }
 
 }
