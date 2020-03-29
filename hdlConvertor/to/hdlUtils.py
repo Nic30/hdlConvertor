2e8ad703ea46a2befa6353b54cd38c35724e40c2
Nic30 <nic30@seznam.cz>
2020-03-29 19:32:05 +0200

    python2.7 compatibility

--- hdlConvertor/to/hdlUtils.py
+++ hdlConvertor/to/hdlUtils.py
@@ -84,7 +84,7 @@ def iter_with_last(it):
 
 if sys.version_info[0] <= 2:
     def is_str(x):
-        return isinstance(x, basestring)
+        return isinstance(x, (basestring, unicode))
 else:
     def is_str(x):
         return isinstance(x, str)
