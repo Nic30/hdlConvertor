2fff270428c8e0b88424553c4cba4451369d2316
Nic30 <nic30@seznam.cz>
2020-04-17 20:01:17 +0200

    hdl._typeDefs doc

--- hdlConvertor/hdlAst/_typeDefs.py
+++ hdlConvertor/hdlAst/_typeDefs.py
@@ -1,20 +1,46 @@
-from enum import Enum
-from typing import Dict, Union, Tuple, List
 from hdlConvertor.hdlAst._bases import iHdlObjWithName
 
-HdlTypeInt = int
-HdlTypeStr = str
-# Verilog real
-HdlTypeFloat = float
-HdlTypeEnum = Enum
+
+class iHdlTypeDef(iHdlObjWithName):
+    """
+    Base class for definitions of HDL types.
+    :note: note that this is not a c-like typedef but the definition of a type itself.
+        c-like typedef is described as shown in examples below.
+        Equivalent of SystemVerilog/vhdl:
+
+        .. code-block:: systemverilog
+
+            typedef wire [7:0] octet;
+
+        .. code-block:: vhdl
+
+            subtype octet is std_logic_vector(7 downto 0); is
+
+        .. code-block:: python
+
+            HdlVariableDef(name="octet", type=HdlTypeType, value=iHdlExpr(wire[7:0]))
+
+    :note: arrays are described as
+
+        .. code-block:: python
+
+            HdlVariableDef("name", HdlTypeType,
+               HdlCall(HdlBuiltinFn.INDEX, [type, array size])
+    """
 
 
-# arrays are described as HdlCall(HdlBuiltinFn.INDEX, (type, array size))
-class HdlTypeBitsDef(iHdlObjWithName):
+class HdlTypeBitsDef(iHdlTypeDef):
     """
     The type which represents bit or bit vector in HDL (std_logic/_vector
     in VHDL, [0:8] in Verilog )
 
+    :ivar ~.msb: index of most significant bit
+    :ivar ~.lsb: index of least significant bit
+    :ivar ~.signed: True for SV/VDHL signed type, False for unsigned, None if not specified
+        (std_logic_vector, wire, reg)
+    :ivar ~.bit_order_bigendian: if True in VHDL std_logic_vector(msb downto lsb) becomes (msb to lsb)
+    :ivar ~.force_vector: if msb==lsb==0 in VHDL this became std_logic
+        instead of std_logic_vector(0 downto 0)
     :ivar ~.states: 2 means that each bit can be 0 or 1
         4 - (0, 1, X, Z) (e.g. Verilog wire)
         9 - (0, 1, X, Z, U, W, L, H, -) (e.g. VHDL std_logic)
@@ -27,18 +53,21 @@ class HdlTypeBitsDef(iHdlObjWithName):
             'L': Weak signal that should probably go to 0
             'H': Weak signal that should probably go to 1
             '-': Don't care.
-    :note: in new you should let lsb=0, it is there only for legacy issues
+
+    :note: in new designs you should let lsb=0, it is there only for legacy issues
     """
     STD_LOGIC_STATES = 9
     WIRE_STATES = 4
-    __slots__ = ["name", "msb", "lsb", "signed", "is_bigendian", "states"]
+    __slots__ = ["name", "msb", "lsb", "signed", "bit_order_bigendian", "states"]
 
     def __init__(self, msb, lsb=0, signed=False):
         super(HdlTypeBitsDef, self).__init__()
         self.msb = msb  # type: int
         self.lsb = lsb  # type: int
-        self.signed = signed  # type: bool
-        self.is_bigendian = False  # type: bool
+        self.signed = signed  # type: Optional[bool]
+        self.bit_order_bigendian = False  # type: bool
+        self.force_vector = True  # type: bool
+        self.is_latched = False  # type: bool
         self.states = 2
 
     def width(self):
@@ -58,14 +87,39 @@ class HdlTypeBitsDef(iHdlObjWithName):
             and self.is_bigendian == other.is_bigendian)
 
 
-class HdlClassDef(iHdlObjWithName):
+class HdlClassDef(iHdlTypeDef):
     """
     Definition of SystemVerilog class/struct/interface or VHDL record
 
+    :ivar ~.parents: parent classes for inheritance
+    :ivar ~.is_virtual: True for for SV abstract (virtual) classes
+    :ivar ~.is_struct: True for SV struct or vhdl record
+    :ivar ~.is_union: True for SV union
+    :ivar ~.private: list of private members
+    :ivar ~.public: list of public members
+    :ivar ~.protected: list of protected members
+
     :note: name may be None
+    :note: equivalent of SystemVerilog/vhdl:
+
+        .. code-block:: systemverilog
+
+            typedef struct packed {
+              int a;
+            } s_name;
+
+        .. code-block:: vhdl
+
+            type s_name is record
+              a    : integer;
+            end record;
+
+        .. code-block:: python
+
+            HdlVariableDef(name="s_name", type=HdlTypeType, value=HdlClassDef(...))
 
     """
-    __slots__ = ["name", "parents", "is_virtual", "is_static",
+    __slots__ = ["name", "parents", "is_virtual",
                  "is_struct", "is_union", "is_interface",
                  "private", "public", "protected"]
 
@@ -73,19 +127,22 @@ class HdlClassDef(iHdlObjWithName):
         super(HdlClassDef, self).__init__()
         self.parents = []  # type: List[iHdlExpr]
         self.is_virtual = False  # type: bool
-        self.is_static = False  # type: bool
         self.is_struct = False  # type: bool
         self.is_union = False  # type: bool
+        self.is_packed = True  # type: bool
         self.private = []  # type: List[iHdlObj]
         self.public = []  # type: List[iHdlObj]
         self.protected = []  # type: List[iHdlObj]
 
 
-class HdlEnumDef(iHdlObjWithName):
+class HdlEnumDef(iHdlTypeDef):
     """
     Definition of VHDL Enumeration Type or SystemVerilog enum
 
     :note: name may be None
+    :note: equivalent of SystemVerilog/vhdl:
+        enum { a, b } e_name; / type e_name is (a, b); is
+        HdlVariableDef(name="e_name", type=HdlTypeType, value=HdlEnumDef("e_name", ["a", "b"]))
     """
     __slots__ = ["name", "values"]
 
