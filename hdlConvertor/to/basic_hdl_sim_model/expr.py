7eba0d0dc8ff9e9cffa4354603abcf6a8c798481
Nic30 <nic30@seznam.cz>
2020-04-24 21:02:35 +0200

    ToBasicHdlSimModelExpr.visit_HdlIntValue: support for str + base

--- hdlConvertor/to/basic_hdl_sim_model/expr.py
+++ hdlConvertor/to/basic_hdl_sim_model/expr.py
@@ -96,14 +96,25 @@ class ToBasicHdlSimModelExpr(ToHdlCommon):
                 w(str(o.val))
             else:
                 b = o.base
-                if b == 2:
-                    f = "0b{0:b}"
-                elif b == 8:
-                    f = "0o{0:o}"
-                elif b == 16:
-                    f = "0x{0:x}"
+                if isinstance(o.val, int):
+                    if b == 2:
+                        f = "0b{0:b}"
+                    elif b == 8:
+                        f = "0o{0:o}"
+                    elif b == 16:
+                        f = "0x{0:x}"
+                    else:
+                        raise NotImplementedError(b)
                 else:
-                    raise NotImplementedError(b)
+                    if b == 2:
+                        f = "0b{0}"
+                    elif b == 8:
+                        f = "0o{0}"
+                    elif b == 16:
+                        f = "0x{0}"
+                    else:
+                        raise NotImplementedError(b)
+
                 w(f.format(o.val))
 
     def visit_iHdlExpr(self, o):
