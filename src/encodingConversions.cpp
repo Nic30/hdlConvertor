2928e407665ad659d8b932dcc20a5a7561ebac61
Nic30 <nic30@seznam.cz>
2020-08-15 09:04:29 +0200

    msvc compatibility

--- src/encodingConversions.cpp
+++ src/encodingConversions.cpp
@@ -25,8 +25,14 @@ std::string _to_utf8(const std::string &str, const std::string &encoding) {
 	} else if (encoding == "utf-16") {
 		std::wstring_convert<std::codecvt_utf8_utf16<char16_t>, char16_t> convert;
 		// because of bug in MSVC https://stackoverflow.com/questions/32055357/visual-studio-c-2015-stdcodecvt-with-char16-t-or-char32-t
-		auto _str = reinterpret_cast<const char16_t*>(str.data());
-		return convert.to_bytes(_str, _str + (str.size() >> 2));
+		#if _MSC_VER >= 1900
+			std::wstring_convert<std::codecvt_utf8_utf16<int16_t>, int16_t> convert;
+			auto _str = reinterpret_cast<const int16_t*>(str.data());
+			return convert.to_bytes(_str, _str + (str.size() >> 2));
+		#else
+			auto & _str = reinterpret_cast<const std::u16string&>(str);
+			return convert.to_bytes(_str);
+		#endif
 	} else if (encoding == "iso-8859-1") {
 		return iso_8859_1_to_utf8(str);
 	} else {
