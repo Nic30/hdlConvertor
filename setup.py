dfbc35831ed0bc09d5d165463116169ebb0ff52f
Nic30 <nic30@seznam.cz>
2020-09-06 22:36:00 +0200

    v2.1

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
         # '-DCMAKE_BUILD_TYPE=Debug'
     ],
     name='hdlConvertor',
-    version='2.0',
+    version='2.1',
     description='VHDL and System Verilog parser written in c++',
     long_description=long_description,
     long_description_content_type="text/markdown",
